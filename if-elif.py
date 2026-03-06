5a39ea1ead521f3159fdc97faed897a9f8e257f7
Moinaruksar <moinaruksar.2004@gmail.com>
2026-03-06 12:55:53 +0530

    Add files via upload

---
+++ if-elif.py
@@ -0,0 +1,120 @@
+#if-elif
+'''a=4
+b=9
+if a!=b:
+    print("not equal")
+elif a==b:
+        print("equal")'''
+
+'''a=4
+b=9
+if a==b:
+    print("not equal")
+elif a>=b:
+        print("equal")
+elif a<b:
+        print("less")'''
+
+
+ #if-elif-else
+
+'''a=40
+b=90
+if a<b:
+    print("less")
+elif a>=b:
+        print("equal")
+else:
+        print("less")'''
+
+
+'''a=40
+b=90
+if a>b:
+    print("less")
+elif a<b:
+        print("equal")
+else:
+    print("less")'''
+
+
+
+'''a = 40
+b = 90
+
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+elif a == b:
+    print("a is equal to b")
+elif a + b > 100:
+    print("sum of a and b is greater than 100")
+elif a % 2 == 0 and b % 2 == 0:
+    print("both numbers are even")
+else:
+    print("none")'''
+
+#multiple-if
+
+'''a=9
+b=12
+if a<b:
+    print("less")
+if b<a:
+      print("greater")
+if a!=b:
+       print("not equal")'''
+
+
+
+a=9
+b=12
+if a==b:
+    print("equal")
+if b<a:
+      print("greater")
+if a!=b:
+       print("not equal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
