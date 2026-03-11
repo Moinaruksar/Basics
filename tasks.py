db8b6765ff63070ebc945a72008261c34a9dbd50
Moinaruksar <moinaruksar.2004@gmail.com>
2026-03-11 12:36:52 +0530

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,88 @@
+#voting
+
+'''age=int(input("age:    "))
+if age>=18:
+    print("ELIGBLE")
+else:
+    print("NOT ELIGIBLE")'''
+
+#even or odd
+
+'''number=int(input("number:   "))
+if number%2==0:
+    print("EVEN")
+else:
+    print("ODD")'''
+
+#vowels
+
+'''n=["a","e","i","o","u"]
+a=input("letter:  ")
+print("enter the alphabet ",n)
+if a in n:
+    print("vowel")
+else:
+    print("consonant")'''
+           
+
+#leap year
+
+'''year=int(input("year:    "))
+if year%4==0:
+    print("leap year")
+else:
+    print("not a leap year")'''
+
+#guest code
+
+'''name=input("name:   ")
+if name=="moina":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+
+'''n=["monu","sonu","riya","joy","lucky"]
+a=input("name:")
+if a in n:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+
+
+'''user = input("Enter user: ")
+password = input("Enter pass: ")
+
+if user == "Moinaruksar":
+    if password == "monu765":
+        print("Login Success")
+    else:
+        print("Invalid Password")
+else:
+    print("Invalid Username")'''
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+          
